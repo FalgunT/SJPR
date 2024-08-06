c22ab8e0484e8ddc44f8d9649b11b0f271cd1a0b
Vidhi Shah <vidhi.shah@gleamglobal.co.uk>
2024-08-06 15:46:16 +0530

    Create delete_confirmation_dialog.dart
    Added Delete Confirmation Dialog

---
+++ sjpr/lib/widgets/delete_confirmation_dialog.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:sjpr/screen/lineitems/line_items_bloc.dart';
+import 'package:sjpr/utils/color_utils.dart';
+import '../common/app_theme.dart';
+
+class DeleteConfirmationDialog extends StatelessWidget {
+  const DeleteConfirmationDialog({
+    super.key,
+    required this.bloc,
+  });
+
+  final LineItemsBloc bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: listTileBgColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.warning_amber_rounded, color: Colors.yellow, size: 50),
+            SizedBox(height: 16),
+            Text(
+              'Delete line',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Are you sure you want to delete this item?',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black, // Background color
+                backgroundColor: Colors.yellow, // Text color
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              ),
+              onPressed: () async {
+                bool res = await bloc.deleteLineItemDetail(
+                    bloc.lineitemdetail.value.id, context);
+                if (res) {
+                  Navigator.of(context).pop(); // Close the dialog
+                  Navigator.of(context).pop(); // Close the page
+                  // Add your delete logic here
+                }
+              },
+              child: Text(
+                'Continue to delete',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text(
+                'Cancel',
+                style: TextStyle(fontSize: 16, color: Colors.yellow),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
